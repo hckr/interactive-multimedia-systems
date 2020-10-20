652b15653da3baa5ed7419db9a6d309ef7fa66a1
Luke <luke@luke.co.pl>
2020-10-21 01:33:37 +0200

    MVP of image quality survey

--- perceptual-image-quality-assessment/web/script.js
+++ perceptual-image-quality-assessment/web/script.js
@@ -1,6 +1,18 @@
-const imageEl = document.querySelector("#image");
+"use strict";
 
-const imageVariants = imageVariantsGenerator("../image-grayscale.png", [
+const imageEl = document.querySelector("#image");
+const descriptionEl = document.querySelector("#description");
+const rankButtonsEl = document.querySelector("#rank-buttons");
+const endScreenEl = document.querySelector("#end-screen");
+const resultTextarea = document.querySelector("#result");
+
+document.querySelector("#call-to-action").onclick = async () => {
+  descriptionEl.classList.add("hidden");
+  rankButtonsEl.classList.remove("hidden");
+  showNextVariant();
+};
+
+const qualityVariants = [
   0.05,
   0.15,
   0.25,
@@ -11,29 +23,59 @@ const imageVariants = imageVariantsGenerator("../image-grayscale.png", [
   0.75,
   0.85,
   0.95,
-]);
+];
+const imageVariants = imageVariantsGenerator(
+  "../image-grayscale.png",
+  qualityVariants
+);
 
-document.querySelectorAll("#rankButtons button").forEach((button) => {
-  button.onclick = async () => {
-    const {value, done} = await imageVariants.next();
-    if (done) {
-      image.src = '';
-      return;
-    }
-    image.src = value;
-  };
-});
+const results = [];
+
+async function showNextVariant() {
+  const {
+    value,
+    done,
+  } = await imageVariants.next();
+
+  if (done) {
+    showEndScreen();
+    return;
+  }
+
+  const { quality, dataUrl } = value;
+
+  imageEl.src = dataUrl;
+
+  rankButtonsEl.querySelectorAll("button").forEach((button, key) => {
+    button.onclick = () => {
+      results.push([quality, key + 1]);
+      console.log(JSON.stringify(results));
+      showNextVariant();
+    };
+  });
+}
+
+function showEndScreen() {
+  rankButtonsEl.classList.add("hidden");
+  imageEl.classList.add("hidden");
+  endScreenEl.classList.remove("hidden");
+  resultTextarea.value = JSON.stringify(results);
+}
+
+resultTextarea.onclick = function() {
+  this.select();
+}
 
 async function* imageVariantsGenerator(origImageSrc, qualityVariants) {
   const dataUrls = shuffle(
-    await prepareImageVariantsAsDataURLs(origImageSrc, qualityVariants)
+    await prepareImageVariants(origImageSrc, qualityVariants)
   );
   for (const dataUrl of dataUrls) {
     yield dataUrl;
   }
 }
 
-function prepareImageVariantsAsDataURLs(origImageSrc, qualityVariants) {
+function prepareImageVariants(origImageSrc, qualityVariants) {
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
 
@@ -45,9 +87,10 @@ function prepareImageVariantsAsDataURLs(origImageSrc, qualityVariants) {
       ctx.drawImage(origImage, 0, 0);
 
       resolve(
-        qualityVariants.map((quality) =>
-          canvas.toDataURL("image/jpeg", quality)
-        )
+        qualityVariants.map((quality) => ({
+          quality,
+          dataUrl: canvas.toDataURL("image/jpeg", quality),
+        }))
       );
     };
     origImage.onerror = reject;
