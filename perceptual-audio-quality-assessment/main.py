8232b6227f7ff80bce59b8c6a48695ff03bfedd5
Luke <luke@luke.co.pl>
2020-11-15 19:19:12 +0100

    Audio quality assessment: save user scores to JSON, save actual scores

--- perceptual-audio-quality-assessment/main.py
+++ perceptual-audio-quality-assessment/main.py
@@ -4,8 +4,10 @@ from audiovariants import QUANTIZATION_VARIANTS, SAMPLING_RATE_VARIANTS
 from matplotlib.figure import Figure
 from tkinter import *
 from tkinter.ttk import Separator
+from tkinter.messagebox import showinfo
 import wave
 import os
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 from glob import glob
@@ -22,6 +24,11 @@ def main():
 
     samples = glob(os.path.join(SAMPLES_PATH, '*.wav'))
     loaded_samples = [load_wav(sample) for sample in samples]
+    user_scores_variables = {
+        # 'group': [
+        #     'id': variable
+        # ]
+    }
 
     if len(samples) == 0:
         print('No samples found')
@@ -43,35 +50,41 @@ def main():
 
     Separator().pack(pady=5, fill='both')
 
-    Button(root, text='Play and plot original (16-bit, 44.1 kHz)', command=lambda: show_plot_and_play(
+    Button(root, text='Play and plot the original (16-bit, 44.1 kHz)', command=lambda: show_plot_and_play(
         selected_sample_np_array(), BASE_SAMPLING_RATE)).pack(fill='both', expand=True)
 
     Separator().pack(pady=5, fill='both')
 
-    for num_bits in QUANTIZATION_VARIANTS:
-        button_with_score(root, text=f'Play and plot quantized to {num_bits}-bit', command=lambda num_bits=num_bits: show_plot_and_play(
-            quantize(selected_sample_np_array(), num_bits), BASE_SAMPLING_RATE, selected_sample_np_array()))
+    user_scores_variables['quantization'] = [
+        (num_bits, button_with_score(root, text=f'Play and plot quantized to {num_bits}-bit', command=lambda num_bits=num_bits: show_plot_and_play(
+            quantize(selected_sample_np_array(), num_bits), BASE_SAMPLING_RATE, selected_sample_np_array())))
+        for num_bits in QUANTIZATION_VARIANTS
+    ]
 
     Separator().pack(pady=5, fill='both')
 
-    for sampling_rate in SAMPLING_RATE_VARIANTS:
-        button_with_score(root, text=f'Play and plot interpolated to {round(sampling_rate/1000)} kHz', command=lambda sampling_rate=sampling_rate: show_plot_and_play(
-            change_sampling_rate(selected_sample_np_array(), BASE_SAMPLING_RATE, sampling_rate), sampling_rate, selected_sample_np_array()))
+    user_scores_variables['sampling_rate'] = [
+        (sampling_rate, button_with_score(root, text=f'Play and plot interpolated to {round(sampling_rate/1000)} kHz', command=lambda sampling_rate=sampling_rate: show_plot_and_play(
+            change_sampling_rate(selected_sample_np_array(), BASE_SAMPLING_RATE, sampling_rate), sampling_rate, selected_sample_np_array())))
+        for sampling_rate in SAMPLING_RATE_VARIANTS
+    ]
 
     Separator().pack(pady=5, fill='both')
 
-    button_with_score(root, text='Play and plot after A-law compression', command=lambda: show_plot_and_play(
-        a_law_compress(selected_sample_np_array(), 2), BASE_SAMPLING_RATE, selected_sample_np_array()))
+    user_scores_variables['compression'] = [
+        ('A-law', button_with_score(root, text='Play and plot after A-law compression', command=lambda: show_plot_and_play(
+            a_law_compress(selected_sample_np_array(), 2), BASE_SAMPLING_RATE, selected_sample_np_array()))),
 
-    button_with_score(root, text='Play and plot after μ-law compression', command=lambda: show_plot_and_play(
-        u_law_compress(selected_sample_np_array(), 2), BASE_SAMPLING_RATE, selected_sample_np_array()))
+        ('u-law', button_with_score(root, text='Play and plot after μ-law compression', command=lambda: show_plot_and_play(
+            u_law_compress(selected_sample_np_array(), 2), BASE_SAMPLING_RATE, selected_sample_np_array()))),
 
-    button_with_score(root, text='Play and plot after ADPCM compression', command=lambda: show_plot_and_play(
-        adpcm_compress(selected_sample_np_array(), 2), BASE_SAMPLING_RATE, selected_sample_np_array()))
+        ('ADPCM', button_with_score(root, text='Play and plot after ADPCM compression', command=lambda: show_plot_and_play(
+            adpcm_compress(selected_sample_np_array(), 2), BASE_SAMPLING_RATE, selected_sample_np_array())))
+    ]
 
     Separator().pack(pady=5, fill='both')
 
-    Button(root, text='Save perceptual score for selected file', command=lambda: print(f'Will save to scores/{selected_sample_name()}.json'),
+    Button(root, text='Save perceptual score for selected file', command=lambda: save_scores(selected_sample_name(), user_scores_variables),
            highlightbackground='cyan').pack(fill='both', expand=True)
 
     root.mainloop()
@@ -122,6 +135,16 @@ def show_plot(current, original):
     plt.tight_layout()
     plt.draw()
     plt.show(block=False)
+
+
+def save_scores(sample_name, user_scores_variables):
+    user_scores = {category: [[variant, int(variable.get())]
+                              for (variant, variable) in scores_variables_list]
+                   for (category, scores_variables_list) in user_scores_variables.items()}
+    file_path = os.path.join('scores', f'{sample_name}.json')
+    with open(file_path, 'w') as f:
+        json.dump(user_scores, f, indent=2)
+    showinfo('Saved', f'Scores saved to {file_path}')
 
 
 if __name__ == '__main__':
